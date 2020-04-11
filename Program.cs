9147dfd3e04938d84defbfb44a181986e9c4ffb4
gidung <gidung.id@gmail.com>
2020-04-11 11:44:22 +0700

    inheritance

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LatihanInheritanceReal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Huda", 19);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Jarwo", 35, 190302, "Guru Matematika");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Budi", 21, 10506, "gidung.id@gmail.com");
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+
+        }
+    }
+}
